6385702ca3cfeb568176f2146326a48cf6259cd5
MengBanana <zhangxinmeng325@gmail.com>
2019-02-26 22:10:00 -0800

    navbar fixed

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -10,7 +10,7 @@ export default class NavBar extends Component {
 
   render() {
     const loginRegLink = (
-      <ul className="navbar-nav">
+      <ul className="navbar-nav ml-auto">
         <li className="nav-item">
           <NavLink className="nav-link" activeClassName="active" to="/login">Login</NavLink>
         </li>
@@ -37,20 +37,54 @@ export default class NavBar extends Component {
     );
 
     return (
-      <div className="container" style={{fontFamily:"Gloria Hallelujah"}}>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="collapse navbar-collapse justify-content-md-center">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <NavLink to="/" className="nav-link">
-                  <h4>Course Advisor</h4>
-                </NavLink>
-              </li>
-            </ul>
+      <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{fontFamily:"Gloria Hallelujah"}}>
+ <a className="navbar-brand" href="/"><h4>Course Advisor</h4></a>
+ <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            
             {localStorage.usertoken ? userLink : loginRegLink}
+            </div>
+            </nav>
           </div>
-        </nav>
-      </div>
+
+/*      <nav style={{fontFamily:"Gloria Hallelujah"}} className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>*/
     );
   }
 }
