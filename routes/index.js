2620e8627c64474e3c70253d159462d8c3b44e4a
Yanwen Huangxu <huangxu.y@husky.neu.edu>
2019-02-27 14:31:08 -0800

    try deploy 4

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res) {
-  //res.render("index", { title: "Express1" });
-  res.render("index.html", { root: __dirname + "../front/build/index.html" } );
+  res.render("index", { title: "Express1" });
+  //res.render("index.html", { root: __dirname + "../front/build/index.html" } );
 });
 
 module.exports = router;
