176e712843aaffe2f168c31c9856bd501a9b890c
Yanwen Huangxu <huangxu.y@husky.neu.edu>
2019-02-27 13:59:14 -0800

    for deploy 2

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function(req, res) {
   //res.render("index", { title: "Express1" });
-  res.sendfile("index.html", { root: __dirname + "../front/public/index.html" } );
+  res.render("index.html", { root: __dirname + "../front/public/index.html" } );
 });
 
 module.exports = router;
